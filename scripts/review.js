9d63b85ad931aad46ae6035d38772482263c4728
Sobomabo Boma Sydney <davidsydney.sb@gmail.com>
2026-06-10 15:26:57 +0100

    push and commit

---
+++ scripts/review.js
@@ -0,0 +1,13 @@
+let reviewCount = Number(localStorage.getItem("reviewCount")) || 0;
+
+reviewCount++;
+
+localStorage.setItem("reviewCount", reviewCount);
+
+document.querySelector("#reviewCount").textContent = reviewCount;
+
+document.querySelector("#currentyear").textContent =
+new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+`Last Modified: ${document.lastModified}`;
